5f29cdbb39b8b1156719882ea03d78572d906c16
Martin Kuenz <martin@kuenz.co.at>
2013-06-11 11:47:59 +0200

    Initial Commit

---
+++ Mobile Rettungskarte/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mobile Rettungskarte
+//
+//  Created by Martin Kuenz on 6/11/13.
+//  Copyright (c) 2013 Martin Kuenz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Mobile RettungskarteTests/Mobile_RettungskarteTests.m
@@ -0,0 +1,36 @@
+//
+//  Mobile_RettungskarteTests.m
+//  Mobile RettungskarteTests
+//
+//  Created by Martin Kuenz on 6/11/13.
+//  Copyright (c) 2013 Martin Kuenz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Mobile_RettungskarteTests : XCTestCase
+
+@end
+
+@implementation Mobile_RettungskarteTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
